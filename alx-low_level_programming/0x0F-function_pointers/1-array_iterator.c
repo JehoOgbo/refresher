659b724a33671ea2956ea5a12ab98e6ae8017d5e
Urom Jehoshaphat <uromjehoshaphat@gmail.com>
2024-12-05 12:25:18 +0100

    1-iterator.c Perform action on each member of an integer array where action is a function pointer

---
+++ alx-low_level_programming/0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function as a given parameter
+ * on each element of an array
+ *
+ * @array: array on whose members the operation must be executed
+ * @size: the size of the array
+ * @action: pointer to the function to be executed on each member of array
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	i = 0;
+	while(i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
